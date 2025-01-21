063eec46106985e4b02356109f8a257e555aa68a
Ahtisham Ahmad <ahtisham548849@gmail.com>
2025-01-21 16:49:46 +0500

    Store Merge in main

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,14 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {create} from 'zustand';
+import {produce} from 'immer';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CoffeeData from '../data/CoffeeData';
+import BeansData from '../data/BeansData';
 
-const Store = () => {
-  return (
-    <View>
-      <Text>Store</Text>
-    </View>
-  );
-};
-
-export default Store;
-
-const styles = StyleSheet.create({});
+const useStore = create(
+  persist(
+    (set, get) => ({
+      CoffeeList: CoffeeData,
+      BeansList: BeansData,
+      FavoriteList: [],
+      CartList: [],
+      OrderHistoryList: [],
+      CartPrice: 0,
+    }),
+    {
+      name: 'coffee-app',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
+export default useStore;
